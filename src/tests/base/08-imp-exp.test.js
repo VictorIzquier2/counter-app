552990ba9faa4749204f9771a7b13ccfdd1d9fd2
VictorIzquier2 <victor.izquierdo1985@gmail.com>
2021-03-05 23:23:16 +0100

    pruebas

--- src/tests/base/08-imp-exp.test.js
+++ src/tests/base/08-imp-exp.test.js
@@ -1,4 +1,4 @@
-import {getHeroeById} from '../../base/08-imp-exp';
+import {getHeroeById, getHeroesByOwner} from '../../base/08-imp-exp';
 import heroes from '../../data/heroes';
 
 describe('Pruebas en funciones de Héroes', () => {
@@ -14,13 +14,40 @@ describe('Pruebas en funciones de Héroes', () => {
 
     console.log(heroe);
   });
-
-  test('Debe de retornar un héroe por id', () => {
+  
+  test('Debe de retornar undefined si héroe no existe', () => {
     
-    const id = 1;
+    const id = 10;
     const heroe = getHeroeById(id);
 
     expect(heroe).toBe(undefined);
   });
+
+  test('Debe de retornar un arreglo con los héroes de DC', () => {
+    // Debe de retornar un arreglo con los héroes de DC 
+    // owner   
+    const owner = 'DC'
+    const heroesFromDC = getHeroesByOwner(owner);
+
+    const heroesData = heroes.filter(h => h.owner === owner);
+
+    //toEqual al arreglo filtrado
+    expect(heroesFromDC).toEqual(heroesData);
+    
+
+  })
+
+  test('Debe de retornar un arreglo con los héroes de Marvel', () => {
+
+    //debe de retornar un arreglo con los héroes de Marvel
+    const owner = 'Marvel'
+    const heroesFromMarvel = getHeroesByOwner(owner);
+
+    const heroesData = heroes.filter(h => h.owner === owner);
+
+    expect(heroesFromMarvel.length).toBe(2);
+    // length = 2
+
+  })
 
 })
